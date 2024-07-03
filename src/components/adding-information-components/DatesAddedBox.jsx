4ce6db9c4b7955a381c544eb2cc6c3d671a4174d
Lina Abuelezz <labuelez@uoguelph.ca>
2024-07-03 11:27:02 -0400

    allowing dynamic box size for add

--- src/components/adding-information-components/DatesAddedBox.jsx
+++ src/components/adding-information-components/DatesAddedBox.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import TempDataContext from "../../hooks/TempDataContext";
 
 const DatesAddedBox = () => {
-    const {tempDateData} = useContext(TempDataContext);
+  const {tempDateData} = useContext(TempDataContext);
+  const boxHeight = 200 + tempDateData.length * 30;
     return (
-        <div className="border-2 border-black w-80 h-80 pl-0 mx-2 rounded-md">
+      <div className="border-2 border-black w-80 h-auto pl-0 mx-2 rounded-md" style={{ height: `${boxHeight}px` }}>
             <h3 className="font-bold ml-1 ">Added Events:</h3>
+            <div className="border border-black mb-1"></div>
         {tempDateData.map((file, index) => (
           <div key={index} className="bg-blue-500 font-mono rounded-sm mb-0.5">
             <p className="ml-2 text-white">{file.newEventName} : {file.newEventDate}</p>
--- src/components/adding-information-components/FilesAddedBox.jsx
+++ src/components/adding-information-components/FilesAddedBox.jsx
@@ -3,9 +3,11 @@ import TempDataContext from "../../hooks/TempDataContext";
 
 const FilesAddedBox = () => {
   const { tempFileData } = useContext(TempDataContext);
+  const boxHeight = 200 + tempFileData.length * 30;
   return (
-    <div className="border-2 border-black w-80 h-80 rounded-md">
+    <div className="border-2 border-black w-80 h-auto pl-0 mx-2 rounded-md" style={{ height: `${boxHeight}px` }}>
       <h3 className="font-bold ml-1">Added Files:</h3>
+      <div className="border border-black mb-1"></div>
       {tempFileData.map((file, index) => (
         <div key={index} className="bg-blue-500 font-mono rounded-sm mb-0.5">
           <p className="ml-2 text-white">
