52df06ab79b081f89af8925b26bb34a3f20d61ad
Lina Abuelezz <labuelez@uoguelph.ca>
2024-07-15 21:53:58 +0300

    trying to fix event routing

--- src/components/homepage-components/DatesDisplay.jsx
+++ src/components/homepage-components/DatesDisplay.jsx
@@ -2,34 +2,36 @@ import HomePageContext from "../../hooks/HomePageContext";
 import BodyPartsContext from "../../hooks/BodyPartsContext";
 import { useContext } from "react";
 
-
 const DatesDisplay = () => {
-    const {bodyParts} = useContext(BodyPartsContext);
-    const {chosenBodyPart} = useContext(HomePageContext);
-    console.log(chosenBodyPart);
-    console.log(bodyParts);
-    // if (!bodyParts || !chosenBodyPart || !bodyParts[chosenBodyPart]) {
-    //     return <p>No files available</p>;
-    // }
+  const { bodyParts } = useContext(BodyPartsContext);
+  const { chosenBodyPart } = useContext(HomePageContext);
+  console.log(chosenBodyPart);
+  console.log(bodyParts);
+  // if (!bodyParts || !chosenBodyPart || !bodyParts[chosenBodyPart]) {
+  //     return <p>No files available</p>;
+  // }
 
-    const handleDateClick = (date) => {
-    
-        const newWindow = window.open(`/event/${date.newEventId}`, '_blank');
-        if (newWindow) {
-          newWindow.eventData = date;
-        }
-      };
+  const handleDateClick = (date) => {
+    const newWindow = window.open(`/event/${date.newEventId}`, "_blank");
+    if (newWindow) {
+      newWindow.eventData = date;
+    }
+  };
   return (
     <div className="bg-white border-2 border-black w-80 h-80 rounded-md ml-4">
       <h3 className="font-bold ml-1">Relevant Dates:</h3>
       <div className="border border-black mb-1"></div>
-      {bodyParts[chosenBodyPart - 1].dates.map(
-        (date, index) => (
-            <div key={index} onClick={() => handleDateClick(date)} className="bg-zinc-400 rounded-sm hover:bg-slate-100 mb-0.5 hover:cursor-pointer">
-                <p className="ml-2 text-white hover:text-black font-mono">{date.newEventName}</p>
-            </div>
-        )
-      )}
+      {bodyParts[chosenBodyPart - 1].dates.map((date, index) => (
+        <div
+          key={index}
+          onClick={() => handleDateClick(date)}
+          className="bg-zinc-400 rounded-sm hover:bg-slate-100 mb-0.5 hover:cursor-pointer"
+        >
+          <p className="ml-2 text-white hover:text-black font-mono">
+            {date.newEventName}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
